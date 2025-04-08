a4bb4519250df590ee2f3a413ecaed91243720d6
zdlpsina <zdlp@sina.cn>
2025-04-08 17:21:13 +0800

    update

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/ThriftServerQueryTestPartFourSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/ThriftServerQueryTestPartFourSuite.scala
@@ -239,7 +239,7 @@ class ThriftServerQueryTestPartFourSuite extends SQLQueryTestPartOneSuite with S
 
     if (!shouldIgnore) {
       val hashMod4 = math.abs(testCase.name.hashCode) % 4
-      val shouldRunTest = hashMod4 == 1 
+      val shouldRunTest = hashMod4 == 1
       if (shouldRunTest) {
         test(testCase.name) {
           runTest(testCase)
